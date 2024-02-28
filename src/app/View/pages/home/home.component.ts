3c89ca1830895403e5934d55f0a5fdd4219670ab
Emmanuel Asabere <emmanuel.asabere@amalitech.com>
2024-02-28 10:47:56 -0800

    feat/ add-new board template

--- src/app/View/pages/home/home.component.ts
+++ src/app/View/pages/home/home.component.ts
@@ -3,13 +3,14 @@ import { DataService } from '../../../controller/data.service';
 import { DashboardFrameComponent } from '../../components/dashboard-frame/dashboard-frame.component';
 import { AddTaskComponent } from "../../modals/add-task/add-task.component";
 import { AddColumnComponent } from "../../modals/add-column/add-column.component";
+import { AddBoardComponent } from "../../modals/add-board/add-board.component";
 
 @Component({
     selector: 'app-home',
     standalone: true,
     templateUrl: './home.component.html',
     styleUrl: './home.component.css',
-    imports: [DashboardFrameComponent, AddTaskComponent, AddColumnComponent]
+    imports: [DashboardFrameComponent, AddTaskComponent, AddColumnComponent, AddBoardComponent]
 })
 export class HomeComponent {
   // constructor(private dataservice: DataService){}
