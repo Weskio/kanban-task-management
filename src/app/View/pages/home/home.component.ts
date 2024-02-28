d63e18fbd69d5fa4a5a76209eccd4de2d091ad9d
Emmanuel Asabere <emmanuel.asabere@amalitech.com>
2024-02-28 10:34:17 -0800

    feat/ add column template

--- src/app/View/pages/home/home.component.ts
+++ src/app/View/pages/home/home.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { DataService } from '../../../controller/data.service';
 import { DashboardFrameComponent } from '../../components/dashboard-frame/dashboard-frame.component';
 import { AddTaskComponent } from "../../modals/add-task/add-task.component";
+import { AddColumnComponent } from "../../modals/add-column/add-column.component";
 
 @Component({
     selector: 'app-home',
     standalone: true,
     templateUrl: './home.component.html',
     styleUrl: './home.component.css',
-    imports: [DashboardFrameComponent, AddTaskComponent]
+    imports: [DashboardFrameComponent, AddTaskComponent, AddColumnComponent]
 })
 export class HomeComponent {
   // constructor(private dataservice: DataService){}
