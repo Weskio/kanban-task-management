51733b5a1fe65cebb7434d9527e71e62215fde08
Emmanuel Asabere <emmanuel.asabere@amalitech.com>
2024-03-01 15:50:45 -0800

    feat/ 26 test cases passed

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,9 +4,28 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        light: {
+          primary: '#007bff',
+          secondary: '#6c757d',
+          background: '#ffffff',
+          text: '#000000',
+        },
+        dark: {
+          primary: '#61dafb',
+          secondary: '#adb5bd',
+          background: '#343a40',
+          text: '#ffffff',
+        },
+      },
+    },
   },
   plugins: [
-    require('tailwind-scrollbar'),
+     require('tailwind-scrollbar'),
   ],
 }
+
+// plugins: [
+//   require('tailwind-scrollbar'),
+// ],
