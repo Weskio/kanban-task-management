f52e7ceb7f3002e52c656227fccb5092aecad22b
Emmanuel Asabere <emmanuel.asabere@amalitech.com>
2024-03-08 16:27:26 -0800

    feat/ columns update

--- src/app/View/components/column/column.component.ts
+++ src/app/View/components/column/column.component.ts
@@ -1,31 +1,31 @@
 import { Component, Input } from '@angular/core';
-import { TaskCardComponent } from "../task-card/task-card.component";
+import { TaskCardComponent } from '../task-card/task-card.component';
 import { DataService } from '../../../controller/data.service';
 import { CurrentBoardService } from '../../../controller/current-board.service';
-import { EmptyColumnComponent } from "../empty-column/empty-column.component";
+import { EmptyColumnComponent } from '../empty-column/empty-column.component';
+import { NgClass } from '@angular/common';
 
 @Component({
-    selector: 'app-column',
-    standalone: true,
-    templateUrl: './column.component.html',
-    styleUrl: './column.component.css',
-    imports: [TaskCardComponent, EmptyColumnComponent]
+  selector: 'app-column',
+  standalone: true,
+  templateUrl: './column.component.html',
+  styleUrl: './column.component.css',
+  imports: [TaskCardComponent, EmptyColumnComponent, NgClass],
 })
 export class ColumnComponent {
+  @Input() columnName!: string;
+  @Input() tasks: any;
+  @Input() taskName!: string;
+  @Input() columnNumber!: number;
+  @Input() columnTotal!: number;
 
-    @Input() columnName!: string
-    @Input() tasks: any
-    @Input() taskName!: string
+  constructor(
+    public dataService: DataService,
+    private currentBoard: CurrentBoardService
+  ) {}
 
-    constructor(public dataService: DataService, private currentBoard: CurrentBoardService) {}
-    
-      getSubtasksDone(task: any): number {
-        return task.subtasks.filter((subtask: any) => subtask.isCompleted === true).length;
-      }
-
-      // ngOnInit(){
-      //   this.dataService.ngOnInit()
-      // }
-    
-
+  getSubtasksDone(task: any): number {
+    return task.subtasks.filter((subtask: any) => subtask.isCompleted === true)
+      .length;
+  }
 }
