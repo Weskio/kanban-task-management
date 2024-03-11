3b5289f0ce3a7a0660e20afe13f4e4ce3d5a190e
Emmanuel Asabere <emmanuel.asabere@amalitech.com>
2024-03-11 13:41:04 -0700

    feat/ register component

--- src/app/View/pages/sign-up/sign-up.component.ts
+++ src/app/View/pages/sign-up/sign-up.component.ts
@@ -2,12 +2,12 @@ import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 @Component({
-  selector: 'app-login',
+  selector: 'app-sign-up',
   standalone: true,
   imports: [RouterLink],
-  templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  templateUrl: './sign-up.component.html',
+  styleUrl: './sign-up.component.css'
 })
-export class LoginComponent {
+export class SignUpComponent {
 
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './View/pages/home/home.component';
 import { LoginComponent } from './View/pages/login/login.component';
+import { SignUpComponent } from './View/pages/sign-up/sign-up.component';
 
 export const routes: Routes = [
     {path:'', component: HomeComponent},
-    {path:'login', component: LoginComponent}
+    {path:'login', component: LoginComponent},
+    {path:'register', component:SignUpComponent}
 ];
