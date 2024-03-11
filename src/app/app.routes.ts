76ac0f420dae5ec927541d0a6b634a954d8607fb
Emmanuel Asabere <emmanuel.asabere@amalitech.com>
2024-03-11 13:18:39 -0700

    feat/login component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './View/pages/home/home.component';
+import { LoginComponent } from './View/pages/login/login.component';
 
 export const routes: Routes = [
-    {path:'', component: HomeComponent}
+    {path:'', component: HomeComponent},
+    {path:'login', component: LoginComponent}
 ];
