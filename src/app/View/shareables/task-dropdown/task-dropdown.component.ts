52e30887b41991f144e46f826ceb51ccf830b302
Emmanuel Asabere <emmanuel.asabere@amalitech.com>
2024-03-12 14:09:15 -0700

    feat/ modals & shareable updated

--- src/app/View/shareables/task-dropdown/task-dropdown.component.ts
+++ src/app/View/shareables/task-dropdown/task-dropdown.component.ts
@@ -2,16 +2,28 @@ import { Component, Input } from '@angular/core';
 import { DropdownToggleService } from '../../../controller/dropdown-toggle.service';
 import { ThemeToggleService } from '../../../controller/theme-toggle.service';
 import { NgClass } from '@angular/common';
+import { DataService } from '../../../controller/data.service';
 
 @Component({
   selector: 'app-task-dropdown',
   standalone: true,
   imports: [NgClass],
   templateUrl: './task-dropdown.component.html',
-  styleUrl: './task-dropdown.component.css'
+  styleUrl: './task-dropdown.component.css',
 })
 export class TaskDropdownComponent {
-  @Input() label:string = ''
+  @Input() label: string = '';
+  columns:any
 
-  constructor(public dropdownToggleService: DropdownToggleService, public themeToggleService: ThemeToggleService ){}
+  constructor(
+    public dropdownToggleService: DropdownToggleService,
+    public themeToggleService: ThemeToggleService, 
+    public dataService: DataService
+  ) {}
+
+  ngOnInit() {
+    this.dataService.getPColumns().subscribe((data: any) => {
+      this.columns = data;
+    });
+  }
 }
